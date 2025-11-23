568c8528d5c554981ade3616dfc7c73797e6d687
Daniil Gnidenko <dagnidenko@gmail.com>
2025-11-23 13:23:19 +0300

    Создание моделей и миграций. Регистрация моделей в админке

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from catalog.models import Category, Product
+
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'id')
+    list_filter = ('name',)
+    search_fields = ('name',)
