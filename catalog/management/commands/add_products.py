d54b1e491e0d104026237ee1b1960f1c861bd7e4
Daniil Gnidenko <dagnidenko@gmail.com>
2025-11-23 21:41:43 +0300

    Добавление кастомной команды

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -1,28 +1,17 @@
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 from catalog.models import Product, Category
 
 
 class Command(BaseCommand):
     help = 'Add test products to the database'
-    Product.objects.all().delete()
-    Category.objects.all().delete()
 
     def handle(self, *args, **kwargs):
-        category1, _ = Category.objects.get_or_create(name='Игры', description='Категория с компьютерными играми')
+        # Удаление существующих данных
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+        self.stdout.write(self.style.WARNING('Все продукты и категории удалены'))
 
-        games = [
-            {'name': 'The Witcher 3: Wild Hunt', 'description': 'Action role-playing game', 'category': category1,
-             "price": 1999},
-            {'name': 'Cyberpunk 2077', 'description': 'Role-playing game', 'category': category1, "price": 2499},
-            {'name': 'Red Dead Redemption 2', 'description': 'Action-adventure game', 'category': category1,
-             "price": 2299},
-            {'name': 'Grand Theft Auto V', 'description': 'Action-adventure game', 'category': category1,
-             "price": 2999},
-        ]
-
-        for game_data in games:
-            game, created = Product.objects.get_or_create(**game_data)
-            if created:
-                self.stdout.write(self.style.SUCCESS(f'Successfully added book: {game.name}'))
-            else:
-                self.stdout.write(self.style.WARNING(f'Book already exists: {game.name}'))
+        # Загрузка данных из фикстуры
+        call_command('loaddata', 'catalog.json')
+        self.stdout.write(self.style.SUCCESS('Данные успешно загружены из фикстуры catalog.json'))
