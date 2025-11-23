dd594b357468b88d18d8b28cef9cd5f5be692286
Daniil Gnidenko <dagnidenko@gmail.com>
2025-11-23 20:10:25 +0300

    Добавление кастомной команды

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,12 +6,12 @@ from catalog.models import Category, Product
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'id')
-    list_filter = ('name',)
-    search_fields = ('name',)
+    list_display = ('id', 'name')
+    search_fields = ('name', 'description')
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'id')
-    list_filter = ('name',)
-    search_fields = ('name',)
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
